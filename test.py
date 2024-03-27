72a45b07a2288a3be89b16bdedffb6a8c00ebe17
Cumametal <163322486+Cumametal@users.noreply.github.com>
2024-03-27 15:35:15 -0600

    Update test.py

--- test.py
+++ test.py
@@ -23,187 +23,187 @@ if 'client_input' not in st.session_state or 'user_name' not in st.session_state
     st.session_state.rfq_inquiry_date = ''
     st.session_state.rfq_mail = ''
     st.session_state.numero_RFQ = ''  # Inicializar numero_RFQ aquí
+    
+if 'tentative_delivery_date' not in st.session_state or 'number_of_operations'not in st.session_state or 'number_of_operations'not in st.session_state or 'selected_fabrication_order'not in st.session_state:
+    st.session_state.tentative_delivery_date= ''
+    st.session_state.number_of_operations= ''
+    st.session_state.selected_fabrication_order= ''
 
 
+# Mostrar dataframes Master_plan and selected_fabrication_order
 
-# Mostrar dataframes RFQ_Control & Clientes_df
-
-col_izq, col_der = st.columns([3, 1])
-
-col_izq.subheader("RFQ control")
+st.subheader("Master Plan")
 rfq_control = conn.read(worksheet="Master_plan",ttl=5)
 rfq_control = rfq_control.dropna(how = 'all')
-col_izq.write(rfq_control.tail(5))
+st.write(rfq_control.tail(15))
 
-col_der.subheader("Control clientes")
-clientes_df = conn.read(worksheet="Datos", ttl=5)
-clientes_df = clientes_df.dropna(how = 'all')
-col_der.write(clientes_df)
+#------------------------------------------------------------------------------------------
 
-# Función para actualizar el DataFrame y obtener el número de RFQ
+# Crear un DataFrame de ejemplo
 
-def actualizar_consecutivo(cliente):
-    print("Cliente ingresado:", cliente)
-    print("Valores en la columna 'cliente':", clientes_df['cliente'].values)
+df = pd.DataFrame(rfq_control)
+
+# Función para extraer valores basados en el estado "Open"
+def extract_values(df):
+    # Filtrar el DataFrame por el estado "Open"
+    open_data = df[df['RFQ_status'] == 'Open']
+    if not open_data.empty:
+        # Guardar todas las columnas necesarias en session_state
+        for column_name in ['purchase_order', 'fabrication_order', 'customer', 'user', 'part_num', 'description']:
+            st.session_state[column_name] = open_data[column_name]
+
+# Llamar a la función para guardar los valores
+extract_values(df)
+
+# Mostrar los datos guardados en un DataFrame
+if 'purchase_order' in st.session_state:
+    df_open_data = pd.DataFrame({
+        'Purchase Order': st.session_state['purchase_order'],
+        'Fabrication Order': st.session_state['fabrication_order'],
+        'Customer': st.session_state['customer'],
+        'User': st.session_state['user'],
+        'Part Number': st.session_state['part_num'],
+        'Description': st.session_state['description']
+    })
+    st.write(df_open_data)
+
+else:
+    st.write("No hay datos con el estado 'Open'.")
+
+# Llamar a la función para extraer los valores
+extract_values(df)
+
+# Mostrar los valores extraídos
+# st.write("Valores extraídos:")
+
+# st.write("purchase_order:", st.session_state.get('purchase_order'))
+# st.write("fabrication_order:", st.session_state.get('fabrication_order'))
+# st.write("customer:", st.session_state.get('customer'))
+# st.write("user:", st.session_state.get('user'))
+# st.write("part_num:", st.session_state.get('part_num'))
+# st.write("description:", st.session_state.get('description'))
+
+
+# Boton de seleccionar orden de fabricación para editar
+
+def fab_order():
+    st.session_state.selected_fabrication_order = st.session_state.fab_order_key
+    st.session_state.fab_order_key = None
+
+if 'fabrication_order' in st.session_state:
+    selected_fabrication_order = st.selectbox('Select Fabrication Order', st.session_state['fabrication_order'].unique(), index=None, placeholder="Selecciona numero de órden de fabricación", on_change=fab_order, key='fab_order_key' )
+    st.write('Selected Fabrication Order:', st.session_state.selected_fabrication_order)
+
+# -------------------------------------------------------------------------------------
+
+#Entrada de fecha para fecha tentativa de entrega 
     
-    # Verificar si el cliente existe en el DataFrame
-    if cliente in clientes_df['cliente'].values:
-        st.write("Cliente encontrado en la base de datos.")
-        # Obtener el índice del cliente en el DataFrame
-        idx = clientes_df.index[clientes_df['cliente'] == cliente].tolist()[0]
-        st.write("Índice del cliente encontrado:", idx)
-        # Incrementar la columna 'consecutivo_de_cliente' en 1
-        clientes_df.at[idx, 'consecutivo_de_cliente'] += 1
-        clientes_df['consecutivo_de_cliente'] = clientes_df['consecutivo_de_cliente'].astype(int)
-        # Actualizar la columna 'orden_RFQ' del registro correspondiente
-        clientes_df.at[idx, 'orden_RFQ'] = f"{clientes_df.at[idx, 'id_cliente']}-{clientes_df.at[idx, 'consecutivo_de_cliente']}"
-        # Guardar el valor actualizado de 'orden_RFQ' en la variable 'numero_RFQ'
-        st.session_state.numero_RFQ = clientes_df.at[idx, 'orden_RFQ']
-        st.success(f"Se ha actualizado el consecutivo para el cliente {cliente}. Número de RFQ: {st.session_state.numero_RFQ}")
-        conn.update(worksheet="clientes_df", data= clientes_df)
-    else:
-        print("Cliente no encontrado en la base de datos.")
-        st.error(f"No se encontró el cliente {cliente} en la base de datos.")
-
-# Función para mostrar orden_RFQ actual
-
-def show_current_ordenRFQ(customer):
-    # Verificar si el cliente existe en el DataFrame
-    if customer in clientes_df['cliente'].values:
-        # Obtener el índice del cliente
-        idx = clientes_df.index[clientes_df['cliente'] == customer].tolist()[0]
-        # Obtener el valor de 'orden_RFQ' correspondiente
-        orden_RFQ = clientes_df.at[idx, 'orden_RFQ']
-        return orden_RFQ
-    else:
-        return None
-
-
-
-# Entrada de dato para cliente_input
-def customer():
-    st.session_state.client_input = st.session_state.customer_key
-    st.session_state.customer_key = None
-
-client_input = st.selectbox('Cliente', ('ETM', 'WOLVENG', 'BOSCH','BRP','UL','CONTROL DIGITAL','3CON','BAKER HUGHES','PLASTICSMART',
-                                         'SAARGUMMI','EPS','NRMACHINING','CRG','KIMBERLY CLARK','DIICSA','DACOM','HARMAN','XOMERTRY',
-                                         'ICARUS','THYSSENKRUPP','SHUNK','IBERFLUID'), index=None, placeholder="Selecciona cliente", key='customer_key', on_change=customer)
-
-st.write(f'Cliente seleccionado: {st.session_state.client_input}')
-
-# Mostrar ultimo numero de orden_RFQ
-
-if st.button("Mostrar Orden RFQ"):
-    orden_RFQ = show_current_ordenRFQ(st.session_state.client_input)
-    if orden_RFQ is not None:
-        st.success(f"El valor actual de orden RFQ para {st.session_state.client_input} es: {orden_RFQ}")
-    else:
-        st.error(f"No se encontró el cliente {st.session_state.client_input} en la base de datos.")
-
-st.divider()
-
-# Entrada de dato para user_name
-
-def username():
-    st.session_state.user_name = st.session_state.user_name_key
-    st.session_state.user_name_key = ''
-
-st.text_input('Nombre y apellido de usuario', key='user_name_key', on_change=username, placeholder="Proporciona nombre + apellido")
-
-st.write(f'Nombre de usuario proporcionado: {st.session_state.user_name}')
-st.divider()
     
-# Entrada de dato para descripcion
+def tentative_deliverydate():
+    st.session_state.tentative_delivery_date = st.session_state.tentative_deliverydate_key
+    st.session_state.tentative_deliverydate_key = None
 
-def description():
-    st.session_state.descripcion = st.session_state.descripcion_key
-    st.session_state.descripcion_key = ''
+tentative_delivery_date = st.date_input("Fecha en que se solicita RFQ", format="DD.MM.YYYY", value=None, key='tentative_deliverydate_key', on_change=tentative_deliverydate)
 
-st.text_input('Descripción', key='descripcion_key', on_change=description, placeholder='Describe la pieza a fabricar')
-
-st.write(f'Descripción proporcionada: {st.session_state.descripcion}')
+st.write(f'Fecha tentativa de entrega {st.session_state.tentative_delivery_date}')
 st.divider()
 
-# Entrada de dato para pm_asignado
-def assigned_pm():
-    st.session_state.pm_asignado = st.session_state.pm_asignado_key
-    st.session_state.pm_asignado_key = None
 
-pm_asignado = st.selectbox('Project Manager asignado', ('Rodrigo Ramirez', 'Elian Sanabria','Sergio Santos'), index=None, placeholder="Selecciona encargado", key='pm_asignado_key', on_change=assigned_pm)
-st.write(f'Representante de CUMA para el proyecto: {st.session_state.pm_asignado}')
-st.divider()
+# Boton de agregar operación nueva e ir incrementando el número de operacion / variable
 
-# Entrada de dato para rfq_inquiry_date
-def inquiry_date():
-    st.session_state.rfq_inquiry_date = st.session_state.rfq_inquiry_date_key
-    st.session_state.rfq_inquiry_date_key = None
 
-rfq_inquiry_date = st.date_input("Fecha en que se solicita RFQ", format="DD.MM.YYYY", value=None, key='rfq_inquiry_date_key', on_change=inquiry_date)
 
-st.write(f'Fecha en que se solicita la cotización {st.session_state.rfq_inquiry_date}')
-st.divider()
+# Entrada de dato para operación
 
-# Entrada de dato para RFQ_email
 
-def email_keywords():
-    st.session_state.rfq_mail = st.session_state.rfq_mail_key
-    st.session_state.rfq_mail_key = ''
 
-rfq_mail = st.text_input("Palabras clave de correo", placeholder="Texto para buscar en correo", key='rfq_mail_key', on_change=email_keywords)
+def create_operation_columns():
+    # Generate column names based on number_of_operations
+    for i in range(1, st.session_state.number_of_operations + 1):
+        process_col = f"process_{i}"
+        operation_status_col = f"operation_status{i}"
+        machine_hr_col = f"machine_hr{i}"
+        labour_hours_col = f"labour_hours{i}"
+        picture_col = f"picture{i}"
 
-st.write(f'Texto clave a buscar en el correo: {st.session_state.rfq_mail}')
-st.divider()
+        # Use the generated column names as needed
+        st.write(process_col, operation_status_col, machine_hr_col, labour_hours_col, picture_col)
 
-# Creación de status como primer status = open
+        # Create text inputs and camera input for each set of columns
+        process_val = st.text_input(f"Process {i}")
+        operation_status_val = st.text_input(f"Operation Status {i}")
+        machine_hr_val = st.text_input(f"Machine HR {i}")
+        labour_hours_val = st.text_input(f"labour_hours{i}")
+        picture_val = st.file_uploader(f"Upload Picture {i}", type=['jpg', 'jpeg', 'png'])
 
-st.session_state.order_status = "Open"
+def update_values():
+    # Update the values for each set of columns
+    for i in range(1, st.session_state.number_of_operations + 1):
+        process_val = st.session_state.get(f"process_input_{i}", "")
+        operation_status_val = st.session_state.get(f"operation_status_input_{i}", "")
+        machine_hr_val = st.session_state.get(f"machine_hr_input_{i}", "")
+        labour_hours_val = st.session_state.get(f"labour_hours_input_{i}", "")
+        picture_val = st.session_state.get(f"picture_input_{i}", "")
 
-st.write("El status de la orden comienza en: ", st.session_state.order_status)
+        # Use the values as needed, for example, print them
+        st.write(f"Process {i}: {process_val}")
+        st.write(f"Operation Status {i}: {operation_status_val}")
+        st.write(f"Machine HR {i}: {machine_hr_val}")
+        st.write(f"labour_hours  {i}: {labour_hours_val}")
+        if picture_val is not None:
+            st.image(picture_val)
+
+def num_of_op():
+    st.session_state.number_of_operations = st.session_state.num_of_op_key
+
+st.number_input("Numero de operaciones requeridas", value=None, key='num_of_op_key', on_change=num_of_op, placeholder="Escribe un número entero...",step= 1)
+st.write(f'Numero de operaciones a cargar: {st.session_state.number_of_operations}')
+
+if st.button("Create Columns"):
+    create_operation_columns()
+
+if st.button("Update Values"):
+    update_values()
+
 
 # Boton para crear RFQ
 
-if st.button("Crear RFQ"):
-     actualizar_consecutivo(st.session_state.client_input)
-     st.success(f"Nuevo número de RFQ para el cliente {st.session_state.client_input}: {st.session_state.numero_RFQ}")
+# if st.button("Crear RFQ"):
+#      actualizar_consecutivo(st.session_state.Datos)
+#      st.success(f"Nuevo número de RFQ para el cliente {st.session_state.Datos}: {st.session_state.numero_RFQ}")
 
 # Mostrar los datos a cargar
 
 st.markdown("<h4 style='text-align: center;'>Datos a cargar </h4>", unsafe_allow_html=True)
 
-new_data = {
-    "RFQ_num": [st.session_state.numero_RFQ],
-    "RFQ_mail": [st.session_state.rfq_mail],
-    "RFQ_inquiry_date": [st.session_state.rfq_inquiry_date],
-    "PM_asignado": [st.session_state.pm_asignado],
-    "Cliente": [st.session_state.client_input],
-    "Usuario":[st.session_state.user_name],
-    "Descripcion": [st.session_state.descripcion],
-    "Status": [st.session_state.order_status]
-}   
+# new_data = {
+#     "RFQ_num": [st.session_state.numero_RFQ],
+#     "RFQ_mail": [st.session_state.rfq_mail],
+#     "RFQ_inquiry_date": [st.session_state.rfq_inquiry_date],
+#     "PM_asignado": [st.session_state.pm_asignado],
+#     "Cliente": [st.session_state.Datos],
+#     "Usuario":[st.session_state.user_name],
+#     "Descripcion": [st.session_state.descripcion],
+#     "Status": [st.session_state.order_status]
+# }   
 
-my_df = pd.DataFrame(new_data)
+#my_df = pd.DataFrame(new_data)
 
-st.write(my_df)
+#st.write(my_df)
 st.warning("Revisar si los datos están correctos para poder cargarlos al sistema y confirmar")
 
 
 #Agregar datos a la base principal RFQ Control
 
-borrar_datos = st.button("Agregar datos" )
+add_data = st.button("Agregar datos" )
 
 
 # Restablecer valores cuando se agregan datos a df
     
-if borrar_datos:
-    rfq_control = rfq_control.append(my_df, ignore_index=True)
-    st.header("New File")
-    st.write(rfq_control.tail(5))
-    conn.update(worksheet="1 rfq control", data= rfq_control)
+# if add_data:
+#     rfq_control = rfq_control.append(my_df, ignore_index=True)
+#     st.header("New File")
+#     st.write(rfq_control)
+#     conn.update(worksheet="1 rfq control", data= rfq_control)
 
     
-    
-
-
-
-
-
